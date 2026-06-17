f108cf6365aaa76eeb97d6d727b63011b778c942
skibidimiku <idkwim19@gmail.com>
2026-06-17 14:18:02 -0600

    Acertijo 8
    La interfaz cambio, se agregaron los movimientos del puzle. Por ultimo se agrego la función de aleatorizar las posiciones, todo lo demás es un cascaron vacío.

---
+++ Proyecto_Progra3/src/proyecto_progra3/Proyecto_progra3.java
@@ -0,0 +1,12 @@
+package proyecto_progra3;
+
+/**
+ *
+ * @author MSI
+ */
+public class Proyecto_progra3 {
+
+    public static void main(String[] args) {
+        new Interface_puzzle();
+    }
+}
